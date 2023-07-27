2786f7ce0206d5519c0ad50453df55fed9ebd9b4
sumanthrancho <140693554+sumanthrancho@users.noreply.github.com>
2023-07-27 21:12:11 +0530

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,3 +1,56 @@
 import streamlit as st
+import random
+st.title('Welcome to Rock , Paper, Sicssors !\n')
+player_win=0
+computer_win=0
+choices=['R','P','S']
+computer=random.choice(choices)
+
+# print(computer)
 st.title("Rancho Labs")
-st.write("Rock-Paper-Sicssor")
+st.header("Rock-Paper-Sicssor")
+
+
+
+player = st.text_input("Enter a choice (Rock (R), Paper (P), Scissors(S)) only capital case :\n")
+
+
+
+
+st.write("You chose {0} and computer chose {1} \n".format(player,computer))
+if(st.button('Submit')):
+  if(player==computer):
+    st.error("Both players selected {}. It is a tie!".format(player))
+
+  elif player =="R":
+    if computer =='S':
+      st.success("Hurray ! , You win")
+      player_win+=1
+    else:
+      st.error('Opps! , You lose')
+      computer_win+=1
+
+  elif player=='P':
+    if computer =='R':
+      st.success("Hurray ! , You win")
+    else:
+      st.error('Opps! , You lose')
+
+  elif player =="S":
+    if computer =='P':
+      st.success("Hurray ! , You win")
+      player_win+=1
+    else:
+      st.error('Opps! , You lose')
+      computer_win+=1
+  else:
+    st.success('Please enter Stone ,paper, scissors')
+
+
+  st.write("Your Score is : "+str(player_win)+"\n")
+  st.write("Computer Score is : "+str(computer_win))
+
+
+# if(st.button('Submit')):
+#     result = name.title()
+#     st.success(result)
